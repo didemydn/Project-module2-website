08f0c30d94bff4175a901ae4a8c8a292dd5dda4b
Didem Cakir <didemydn@gmail.com>
2023-06-26 21:53:11 +0200

    user post

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,13 +6,14 @@ const router = express.Router();
 
 const User = require ("../models/User.model")
 
-/* GET connect page*/
+
+/* GET connect page SIGN UP AND LOGIN*/ 
 
 router.get("/connect", (req, res, next) => {
     res.render("user/connect")
 });
 
-/* POST */
+/* POST SIGN UP*/
 
 router.post("/connect", (req,res,next) =>{
     console.log("req.body", req.body)
@@ -41,5 +42,37 @@ router.post("/connect", (req,res,next) =>{
     .catch(error => next(error));
 
 });
+
+/* POST LOG IN*/
+
+router.post('/connect', (req, res) => {
+    console.log('req.body', req.body)
+    const {email, password} = req.body;
+    if(!username || !password){
+        res.render('user/connect', {
+            errorMessage: 'Please enter both, email and password to login'
+        })
+        return;
+    }
+
+    User.findOne({email})
+    .then(user => {
+        console.log('user', user)
+        if (!user) {
+            res.render('user/connect', {
+                errorMessage: 'Email is not registered. Please sign up'
+            })
+            return;
+        }
+        else if (bcrypt.compareSync(password, user.passwordHash)){
+        res.render('user/connect', user)}
+        else { 
+            res.render('user/connect', { errorMessage: 'Incorrect password.' });
+          } 
+    })
+    .catch(error => next(error));
+})
+
+
 
 module.exports = router;
