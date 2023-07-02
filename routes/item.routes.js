6b5be304d97ef53003113eacece8b78ccdc1a78d
Didem Cakir <didemydn@gmail.com>
2023-07-02 22:45:38 +0200

    donations

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -2,13 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Item = require('../models/Item.model');
 const upload = require('../config/cloudinary.config');
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard');
 
-// Route for the donate item form
+// GET Routes
+
 router.get('/donate', (req, res) => {
-  res.render('item/donate'); // Render the donate item form (donate-item.hbs)
+  res.render('item/donate'); 
 });
 
-// Route for handling the form submission
+router.get('/connect/mydonations', (req, res) => {
+    Item.find()
+    .then((itemsFromDB) => res.render("item/donations", { item: itemsFromDB }))
+    .catch((err) => console.log(`Error while getting items from the DB: ${err}`));
+});
+  
+
+router.get('/connect/favorite', (req, res) => {
+    res.render('item/favorite-items'); 
+  });
+
+router.get('/donate/edit', (req, res) => {
+    res.render('item/edit-item'); 
+  });
+ 
+
+
+// POST donate form
 router.post('/donate', upload.single('picture'), async (req, res) => {
   try {
     const { title, category, type, size, condition, location, email, phone } = req.body;
@@ -36,5 +55,29 @@ router.post('/donate', upload.single('picture'), async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+// EDIT item
+
+router.post('/donate/edit', isLoggedIn, (req, res) => {
+    const { title, category, type, size, condition, location, email, phone } = req.body; // Update the user's profile details in the database
+    Item.findOneAndUpdate(
+      { _id: req.session.currentUser._id },
+      { title, category, type, size, condition, location, email, phone },
+      { new: true }
+    )
+      .then(updatedItem => {
+        req.session.currentUser = updatedItem; // Update the session with the new user details
+        res.redirect('/connect/profile');
+      })
+      .catch(err => {
+        console.log(err);
+        res.render('item/edit-item', {
+          errorMessage: 'Failed to update the item. Please try again.',
+          
+        });
+      });
+  });
+
+// DELETE item 
 
 module.exports = router;
